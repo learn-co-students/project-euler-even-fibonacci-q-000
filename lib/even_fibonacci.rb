94a746294b67a7aa0731d475127a1d538e3bafb5
Ian Miller <irmiller22@gmail.com>
2014-10-28 09:41:04 -0400

    updated solution

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -3,6 +3,7 @@ def even_fibonacci_sum(limit)
   i, first, second = 0, 0, 1
   while i < limit
     i = first + second
+    break if i > limit
     filtered_array << i if i.even?
     first = second
     second = i
