6eafcf800c6866bc1ea5a7be7c2cdf6f6063b99d
Shmuwol <shmully.wolfson@gmail.com>
2015-11-12 17:54:14 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num)
+  previous = 0
+  current = 1
+  sum = 0
+
+  while current <= num
+    sum += current if current.even?
+    current, previous = previous, previous + current
+  end
+    sum
+end
+
+
+
