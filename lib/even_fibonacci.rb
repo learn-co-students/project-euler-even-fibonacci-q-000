b695fe7ddd5d9925745a490c4c2e54917462fb83
ed-karabinus <ejkarabin@gmail.com>
2015-11-19 13:02:31 -0600

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  previous_fibonacci_1 = 1
+  previous_fibonacci_2 = 1
+  current_fibonacci = 2
+  sum = 0
+  until current_fibonacci > limit
+    if current_fibonacci % 2 == 0
+      sum += current_fibonacci
+    end
+    previous_fibonacci_1 = previous_fibonacci_2
+    previous_fibonacci_2 = current_fibonacci
+    current_fibonacci = previous_fibonacci_1 + previous_fibonacci_2
+  end
+  sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    previous_fibonacci_1 = 1
+    previous_fibonacci_2 = 1
+    current_fibonacci = 2
+    sum = 0
+    until current_fibonacci > @limit
+      if current_fibonacci % 2 == 0
+        sum += current_fibonacci
+      end
+      previous_fibonacci_1 = previous_fibonacci_2
+      previous_fibonacci_2 = current_fibonacci
+      current_fibonacci = previous_fibonacci_1 + previous_fibonacci_2
+    end
+    sum
+  end
+end
