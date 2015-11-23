5b15580a1644b4991387326bb6a19685ff320080
retraido <enzosarin@gmail.com>
2015-11-23 16:36:26 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  new_item = []
+  curr = 0
+    next_num = 1
+    temp = 0
+  while next_num < limit
+        temp = curr + next_num
+        curr = next_num
+        next_num = temp
+    if next_num % 2 == 0 && next_num < limit
+  then new_item << next_num
+end
+end
+new_item.inject(:+)
+end
+
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,31 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def limit
+    @limit
+  end
+
+  def limit=(limit)
+    @limit = limit
+  end
+
+  def sum
+  new_item = []
+  curr = 0
+    next_num = 1
+    temp = 0
+  while next_num < @limit
+        temp = curr + next_num
+        curr = next_num
+        next_num = temp
+    if next_num % 2 == 0 && next_num < @limit
+  then new_item << next_num
+end
+end
+new_item.inject(:+)
+end
+
+  end
