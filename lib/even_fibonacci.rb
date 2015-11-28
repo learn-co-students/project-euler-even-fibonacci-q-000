a5e8766f8a9260836e2603ba19935badc9931485
MitulMistry <mitul.mistry1@gmail.com>
2015-11-28 23:15:45 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  array = [1, 2]
+  i = 0
+
+  loop do
+    i = array[array.size - 1] + array[array.size - 2]
+    if i < limit
+      array << i
+    else
+      break
+    end
+  end
+  #puts array
+
+  sum = 0
+  array.each do |i|
+    if i.even?
+      sum += i
+    end
+  end
+  #puts "SUM: #{sum}"
+  return sum
+end
+
+#even_fibonacci_sum(100)
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @array = [1, 2]
+    i = 0
+
+    loop do
+      i = @array[@array.size - 1] + @array[@array.size - 2]
+      if i < limit
+        @array << i
+      else
+        break
+      end
+    end
+
+  end
+
+  def sum
+    sum = 0
+    @array.each do |i|
+      if i.even?
+        sum += i
+      end
+    end
+    return sum
+  end
+
+end
