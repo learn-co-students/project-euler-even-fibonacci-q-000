466e0dbb452c21374197f65d3737c9b8f948b55c
lizdenhup <lizdenhup@gmail.com>
2015-12-05 22:26:43 -0600

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  fib_array = [1,1]
+  
+  until (fib_array[-1] + fib_array[-2]) >= limit do 
+    fib_array.push(fib_array[-1] + fib_array[-2])
+  end
+
+  fib_array.select { |num| num.even? }.inject { |sum, n| sum + n}
+
+end
+
