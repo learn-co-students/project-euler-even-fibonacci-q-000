647b4267762428e81805702f515347a15b449367
CodrinMihaila <codrin@my.com>
2015-12-07 14:59:02 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  number1 = 1
+  number2 = 2
+  sum = 0
+  while number1 < limit
+  
+    if number1 % 2 == 0
+        sum = sum + number1
+    end
+   new_number = number1 + number2
+   number1 = number2
+   number2 = new_number
+  end
+   return sum
+end
