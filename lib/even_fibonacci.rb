8935b18688b2f88f678861f1da1c91273865a4eb
Rafael A. Mejia <rafael@dataminded.net>
2015-12-08 05:05:27 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  summable = Array.new
+  num1, num2 = 1, 2
+  while num2 < limit do
+    if num2 % 2 == 0
+        summable.push(num2)
+    end
+     num1, num2 = num2, num1 + num2
+  end
+  return summable.reduce(:+)
+end
+
+
