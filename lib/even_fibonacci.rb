a8bc309ec6cf7f3917145c95b8d42e4d9f81cda3
Eric An <erican@me.com>
2015-12-10 17:00:44 -0800

    complete Euler - even Fibonacci numbers

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+a = [1,2]
+
+while a[-2] + a[-1] < limit
+  a << a[-2] + a[-1] 
+end
+
+sum = 0
+a.each { |x| sum += x if x.even? }
+
+sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+	
+	def initialize(limit)
+		@limit = limit
+		@sum = sum
+	end
+
+	def sum
+	a = [1,2]
+
+	while a[-2] + a[-1] < @limit
+	  a << a[-2] + a[-1] 
+	end
+
+	@sum = 0
+	a.each { |x| @sum += x if x.even? }
+
+	@sum
+	end
+
+end
