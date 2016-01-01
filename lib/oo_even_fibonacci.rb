23c56dce3def089700d95aa74acfafdf8d3f83b1
drywaterr24 <angelolea24@gmail.com>
2016-01-01 15:57:28 -0500

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,38 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+    @fibo = fibo
+    @even_fibo = even_fibo
+  end
+
+  def fibo
+    fibo = [1, 2]
+    x = 0
+    y = 1
+
+    loop do
+      z = fibo[x] + fibo[y]
+      fibo << z
+      x += 1
+      y += 1
+      if fibo.last > @limit
+        fibo.pop
+        break
+      end
+    end
+    fibo
+  end
+
+  def even_fibo
+    @fibo.select{|x| x.even?}
+  end
+
+  def sum
+    @even_fibo.reduce(:+)
+  end
+
+end
+
+
