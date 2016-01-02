dcf7da839115d2eede00d8289cc9f7e27e121d53
bizworld <bizworld@outlook.com>
2016-01-02 17:05:51 -0500

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+  fib_array = [1, 2]
+  sum = 0
+  n = 2
+  while (fib_array[n - 2] + fib_array[n - 1]) < @limit
+    fib_array.push(fib_array[n - 2] + fib_array[n - 1])
+    n += 1
+  end
+  fib_array.each do |e| 
+    if e % 2 == 0
+      sum += e
+    end
+  end
+  sum
+end
+
+end
