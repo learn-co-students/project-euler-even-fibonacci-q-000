e52870d8096799951e8476203b2c8fd6fda2be5e
justin071203 <justinmaxwell0502@gmail.com>
2016-01-05 22:33:25 -0600

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+
+
+def fib(n)
+  if n == 0 || n == 1
+    n
+  else
+    fib(n-2) + fib(n-1)
+  end
+end
+
+def fib_array(n)
+  t_array = (2..(n.to_i + 1)).to_a
+  t_array.select! {|x| x <= 34}
+  t_array.map {|x| fib(x)}
+end
+
+def fib_array_evens(n)
+  fib_evens = fib_array(n).select {|x| x % 2 == 0 && x <= n.to_i && x <= 4000000}
+end
+
+def even_fibonacci_sum(n)
+  fib_array_evens(n).inject(0) {|r, e| r + e}
+end
