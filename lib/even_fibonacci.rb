5ad03367c201fd27c0e5e250d918ac9ba032bcab
alexbarron <alexbarron@gmail.com>
2016-01-09 10:14:33 -0800

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num)
+  fibonacci_set = [1,2]
+  while fibonacci_set.last < num
+    if fibonacci_set.last + fibonacci_set[-2] < num
+      fibonacci_set << fibonacci_set.last + fibonacci_set[-2]
+    else
+      break
+    end
+  end
+  even_fibonacci_set = fibonacci_set.select {|i| i.even?}
+  even_fibonacci_sum = 0
+  even_fibonacci_set.each { |x| even_fibonacci_sum += x }
+  return even_fibonacci_sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(num)
+    @num = num
+  end
+
+  def sum
+    fibonacci_set = [1,2]
+    while fibonacci_set.last < @num
+      if fibonacci_set.last + fibonacci_set[-2] < @num
+        fibonacci_set << fibonacci_set.last + fibonacci_set[-2]
+      else
+        break
+      end
+    end
+    even_fibonacci_set = fibonacci_set.select {|i| i.even?}
+    even_fibonacci_sum = 0
+    even_fibonacci_set.each { |x| even_fibonacci_sum += x }
+    return even_fibonacci_sum
+  end
+end
