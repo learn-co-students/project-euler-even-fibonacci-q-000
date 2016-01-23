2ba4b5e46d82d25e9b74e40f1b6238b7777cfce7
mhnguyen289 <mhnguyen289@gmail.com>
2016-01-22 17:39:39 -0800

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,23 @@
+
+class EvenFibonacci
+  def initialize(limit_int)
+    @limit = limit_int
+  end
+
+  def sum()
+    multiple = [1,2,3]
+    counter = 2
+    sum = 2
+    fibonacci = 3
+    while (fibonacci < @limit) do
+      counter += 1
+      multiple << (multiple[counter-1] + multiple[counter-2])
+      fibonacci = multiple[counter]
+      if (fibonacci%2==0 && fibonacci < @limit)
+        sum += fibonacci
+      end
+    end
+    return sum
+  end
+end
 # Implement your object-oriented solution here!
