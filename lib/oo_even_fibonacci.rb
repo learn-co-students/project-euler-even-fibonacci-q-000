3ad6699e05159b29577c045803773c4b1060c974
Lady Codington <myemailforcoding@gmail.com>
2016-01-25 16:58:58 -0500

    No Memoization but this would be the chance to do it

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,2 +1,19 @@
-class EvenFibonacciSum
+class EvenFibonacci
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    total = 0
+    first, second = 0,1
+    while second < limit
+      first, second = second, first + second
+      if first % 2 == 0
+        total += first
+      end
+    end
+    total
+  end
 end
