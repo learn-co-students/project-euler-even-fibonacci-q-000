2b1fc0b3770124760808b3dda30d3a68dc61f53f
Lady Codington <myemailforcoding@gmail.com>
2016-01-25 18:44:11 -0500

    added comments to help beginners

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -2,8 +2,10 @@ def even_fibonacci_sum(limit)
   total = 0
   first, second = 0,1 
   while second < limit
+    # first gets assigned to second and second gets first + second
     first, second = second, first + second
-    if first % 2 == 0
+    # even numbers are added to total
+    if first % 2 == 0 
      total += first
     end
   end
