f39444527a4a0a20aa4fb4be9b7e17d5b13c34be
Lady Codington <myemailforcoding@gmail.com>
2016-01-25 15:14:44 -0500

    simplifying

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,18 +1,5 @@
-# def fibo(n, cache={})
-#   if n == 0 || n == 1
-#     return n
-#   end
-#   cache[n] ||= fibo(n-1, cache) + fibo(n-2, cache)
-# end 
-@terms = Array.new
-
 def even_fibonacci_sum(n)
   return n if n < 2
-  if recursion % 2 == 0
-    
-  end
-end
-
-def recursion
+  return n if n.odd?
   even_fibonacci_sum(n-1) + even_fibonacci_sum(n-2)
 end
