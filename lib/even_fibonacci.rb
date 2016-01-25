d4f9f642c819a5ff480e5b100a0f4f0d053f9479
Lady Codington <myemailforcoding@gmail.com>
2016-01-25 12:56:36 -0500

    undefined method  for nil:NilClass - still working out bug

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,35 @@
-# Implement your procedural solution here!
+# def fibo(n, cache={})
+#   if n == 0 || n == 1
+#     return n
+#   end
+#   cache[n] ||= fibo(n-1, cache) + fibo(n-2, cache)
+# end 
+@cache = [0,1]
+
+def even_fibonacci_sum(limit)
+  terms = Array.new
+  (1..limit).each do |n|
+    term = fibo(n)
+    if term.even?
+      terms << term
+    end
+  end
+  sum(terms)
+end
+
+def sum(terms)
+  total = 0
+  terms.each { |n| total += n }
+  total
+end
+
+def fibo(n)
+  # check if result is in cache
+  # if it is then return it, otherwise do a calculation and save result
+  return @cache[n] if @cache[n]
+
+  # calculation
+  @cache[n] = fibo(n-1) + fibo(n-2)
+end
+
+
