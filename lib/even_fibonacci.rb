f89aff90b7d410bf79f4577aa0b935bf4b451f08
Brian Holland <beehollander@gmail.com>
2016-02-03 21:27:59 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(soft_limit)
+  hard_limit=4e6
+
+  fibs = [1,2]
+  even_fibs = Array.new(1, 2)
+
+  while fibs.last<=soft_limit && fibs.last<=hard_limit
+    fibs.push(fibs[-1] + fibs[-2])
+    if fibs[-1]%2==0
+      even_fibs.push(fibs[-1])
+    end
+  end
+
+  if even_fibs.last > soft_limit
+    even_fibs.pop
+  end
+  return even_fibs.reduce(0, :+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+# Implement your procedural solution here!
+class EvenFibonacci
+  def initialize(soft_limit)
+    @soft_limit=soft_limit
+  end
+  #Helper Methods
+  def sum
+    hard_limit=4e6
+
+    fibs = [1,2]
+    even_fibs = Array.new(1, 2)
+
+    while fibs.last<=@soft_limit && fibs.last<=hard_limit
+      fibs.push(fibs[-1] + fibs[-2])
+      if fibs[-1]%2==0
+        even_fibs.push(fibs[-1])
+      end
+    end
+
+    if even_fibs.last > @soft_limit
+      even_fibs.pop
+    end
+    return even_fibs.reduce(0, :+)
+  end
+end
