d4a2dabc4c6ac995a70e688e9ce5f2c1b319f79c
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-23 23:00:27 -1000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,36 @@
 # Implement your procedural solution here!
+def fibonacci_generator(limit)
+  current_num = 1
+  next_num = 2
+  temp_num = 1
+  fib_array = []
+
+  while current_num < limit
+    fib_array.push(current_num)
+    temp_num = current_num
+    current_num = next_num
+    next_num = next_num + temp_num
+
+  end
+
+  return fib_array
+end
+
+
+def even_fibonacci_sum(limit)
+  all_fibs = fibonacci_generator(limit)
+  
+  even_fibs = all_fibs.select do |item|
+    item % 2 == 0
+  end
+
+  sum_of_fibs = 0
+  even_fibs.each do |item|
+    sum_of_fibs += item
+  end
+
+  return sum_of_fibs
+end
+
+
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,38 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def fibonacci_generator
+    current_num = 1
+    next_num = 2
+    temp_num = 1
+    fib_array = []
+
+    while current_num < @limit
+      fib_array.push(current_num)
+      temp_num = current_num
+      current_num = next_num
+      next_num = next_num + temp_num
+    end
+
+    return fib_array
+  end
+
+  def sum
+    all_fibs = fibonacci_generator()
+    
+    even_fibs = all_fibs.select do |item|
+      item % 2 == 0
+    end
+
+    sum_of_fibs = 0
+    even_fibs.each do |item|
+      sum_of_fibs += item
+    end
+
+    return sum_of_fibs
+  end
+end
