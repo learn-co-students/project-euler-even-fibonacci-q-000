d2bf5478ad3fef400ea69ed46c49bfa2782e907a
bmolina-nyc <bruce.d.molina@gmail.com>
2016-02-27 09:07:42 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(limit)
+
+  fibo_array = [ 1, 2 ]
+  until fibo_array.last + fibo_array[-2] > limit 
+    fibo_array << fibo_array.last + fibo_array[-2]
+  end
+    fibo_array.select { |num| num.even? }.inject { |x,y| x + y }
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+
+
+class EvenFibonacci
+
+  attr_writer :arr
+  attr_reader :limit 
+
+  def initialize(limit)
+    @arr = [1,2]
+    @limit = limit 
+  end
+
+  def evens_only
+    while @arr.last + @arr[-2] < limit
+    @arr << @arr.last + @arr[-2]
+    end
+    @arr.select { |num| num.even? }
+  end 
+
+  def sum
+    evens_only.inject { |x,y| x + y }
+  end
+
+end
