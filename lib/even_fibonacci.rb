3ac45a3442f75a9c20159d8af292edc445ba95ad
anorico <allan.norico@gmail.com>
2016-03-27 19:14:37 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+
+  fibonacci_array = Array.new(2,1)
+  sum=0
+  i=1
+
+  while fibonacci_array.last < limit
+    fibonacci_array << (fibonacci_array[i-1] + fibonacci_array[i])
+    i += 1
+    #p fibonacci_array
+  end
+
+  fibonacci_array.select do |i|
+   if i < limit && i % 2 == 0
+    sum += i
+   end
+  end
+  return sum 
+end
