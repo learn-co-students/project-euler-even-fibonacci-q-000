54ef12a4cb1b88e9a147367cf95a2fe476500ba4
ollieshmollie <olishmollie@gmail.com>
2016-04-21 13:32:28 -0400

    Update oo_even_fibonacci.rb

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,15 +1,16 @@
 # Implement your object-oriented solution here!
 class EvenFibonacci
-  def initialize(limit = nil)
+  def initialize(limit)
     @limit = limit
+    @sequence = [1,2]
   end
 
   def sum
-    sequence = [1,2]
-    while sequence[-1] < @limit
-      sequence.push(sequence[-1] + sequence[-2])
+    while @sequence[-1] < @limit
+      @sequence.push(@sequence[-1] + @sequence[-2])
     end
-    return sequence.keep_if{|num| num.even? && num < @limit}.reduce(:+)
+    @sequence.keep_if{|num| num.even? && num < @limit}.reduce(:+)
   end
 end
+
 
