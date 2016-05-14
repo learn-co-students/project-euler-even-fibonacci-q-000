09228280fc403a7ed5c8b2fa61f7b88926810962
John Anthony Rivera <askinganthony@gmail.com>
2016-05-14 10:38:16 -0700

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,27 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_accessor :limit
+
+  def initialize(limit)
+    self.limit = limit
+  end
+
+  def fibonacci_values_by_limit
+    values = [1]
+    new_value = 2;
+    until (new_value >= self.limit) do
+      values << new_value
+      new_value = values[-1] + values[-2]
+    end
+    values
+  end
+
+  def sum
+    fibonacci_values_by_limit.inject(0) do |sum, n|
+      inject_value = 0
+      inject_value = n if (n % 2 == 0)
+      sum + inject_value
+    end
+  end
+
+end
