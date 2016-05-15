d94684510c105d1b274e292e1909b5443e58e339
Tom Schaeper <schaeper@hyperverse.com>
2016-05-15 17:06:51 -0500

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+  def sum
+
+    f1 = 1
+    f2 = 2
+    sum = 0
+    until f2 >= @limit
+      if f2 % 2 == 0
+        sum += f2
+      end
+      f3 = f1 + f2
+      f1 = f2
+      f2 = f3
+    end
+    sum
+  end
+
+end
