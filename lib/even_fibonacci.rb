7f78c3017a6f81469078ea48a30ba0ddc6ddbdca
Yohanes Wibowo <john.paulo@ymail.com>
2016-05-16 19:03:58 +0700

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num)
+  board = []
+  f1 = 0
+  f2 = 1
+  fn = 0
+while fn < num
+  fn = f1 + f2
+  break if fn > num
+  board << fn if fn.even?
+  f1 = f2
+  f2 = fn
+end
+board.inject(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(num)
+    @f1 = 0
+    @f2 = 1
+    @board = []
+  end
+
+  def even_fibonacci_sum
+    while fn < num
+      fn = @f1 + @f2
+      break if fn > num
+      board << fn if fn.even?
+      @f1 = @f2
+      @f2 = @fn
+    end
+    @board.inject(:+)
+  end
+end
