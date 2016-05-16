8910109d9a0704ad99885d0bab81d2e6ee737fdc
Yohanes Wibowo <john.paulo@ymail.com>
2016-05-16 19:27:40 +0700

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -4,16 +4,22 @@ class EvenFibonacci
     @f1 = 0
     @f2 = 1
     @board = []
+    @collect = even_fibonacci_collect(num)
   end
 
-  def even_fibonacci_sum
-    while fn < num
+  def even_fibonacci_collect(num)
+    fn = 0
+    while fn <= num
       fn = @f1 + @f2
       break if fn > num
-      board << fn if fn.even?
+      @board << fn if fn.even?
       @f1 = @f2
-      @f2 = @fn
+      @f2 = fn
     end
-    @board.inject(:+)
+    @board
+  end
+
+  def sum
+    @collect.inject(:+)
   end
 end
