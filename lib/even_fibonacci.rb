159330f5979a70deb567e5b5d8c385ddbda88285
alexseth1124 <alexseth1124@gmail.com>
2016-06-01 22:35:02 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,5 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  array = [1, 2]
+  (3..limit).each {|number| array << number if number == array[-2] + array[-1]}
+  array.delete_if {|number| number.odd?}.inject(0, :+)
+end
