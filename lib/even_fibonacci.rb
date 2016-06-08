8e6eb8d36788c0a446678f52b0944337ac7a262f
uu <giladbarkan@live.com>
2016-06-07 22:44:50 -0400

    passed tests

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(n)
+  t1 = 0
+  t2 = 1
+  sum = 0
+  while t1 + t2 < n
+    fib = t1 + t2
+    if fib & 1 == 0
+      sum += fib
+    end
+    t1 = t2
+    t2 = fib
+  end
+ 
+  sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(n)
+    @n = n
+  end
+
+  def sum
+    t1 = 0
+    t2 = 1
+    sum = 0
+    while t1 + t2 < @n
+      fib = t1 + t2
+      if fib & 1 == 0
+        sum += fib
+      end
+      t1 = t2
+      t2 = fib
+    end
+     
+    sum
+  end
+end
