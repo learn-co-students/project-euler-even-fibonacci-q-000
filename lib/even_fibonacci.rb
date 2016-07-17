3baf19b9bf438ac5c5f4cc98a982058272aac28d
the-widget <brennenawana108ny@gmail.com>
2016-07-17 09:10:54 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  sequence = [1, 2]
+  cnt1 = 0
+  cnt2 = 1
+  limit.times do 
+    if sequence[cnt1] + sequence[cnt2] < limit
+      sequence.push(sequence[cnt1] + sequence[cnt2])
+      cnt1 += 1
+      cnt2 += 1 
+    end
+  end
+  total = sequence.select{|num| num % 2 == 0}
+  return total.inject(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,24 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    sequence = [1, 2]
+    cnt1 = 0
+    cnt2 = 1
+    @limit.times do 
+      if sequence[cnt1] + sequence[cnt2] < @limit
+        sequence.push(sequence[cnt1] + sequence[cnt2])
+        cnt1 += 1
+        cnt2 += 1 
+      end
+    end
+    total = sequence.select{|num| num % 2 == 0}
+    return total.inject(:+)
+  end
+
+end
+
+# Very inefficient logic. This will loop through many
+# more times than is necessary.
