4d5f2c3ddde13d91f668cb396b6db6ec1f7a1e16
Eoghan Leddy <eleddy521@gmail.com>
2016-07-29 05:51:35 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+
+
+def even_fibonacci_sum(limit)
+
+  fibo_array = Array.new(2, 1)
+  until(fibo_array[0] > limit)
+      fibo_array.unshift(fibo_array[0] + fibo_array[1])
+    
+
+  end
+  fibo_sum = 0
+  fibo_array.each do |x|
+    if(x % 2 == 0 && x < limit)
+      fibo_sum += x
+    end
+  end
+  fibo_sum
+
+end
