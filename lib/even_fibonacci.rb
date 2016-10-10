62d1900cf8b36039aceeb1c330ef48dfcfb6e496
Margaret <eutopia@gmail.com>
2016-10-10 16:53:26 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,11 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+	even_fibonacci_numbers(limit).inject(0) {|sum, x| sum + x}
+end
+
+def even_fibonacci_numbers(limit)
+	array = [1, 2]
+	while array[-1] < limit
+		 array << (array[-1] + array[-2])
+	end
+	array.select {|num| num.even? && num < limit}
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,19 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+	def initialize(limit)
+		@limit = limit
+	end
+
+	def sum
+		even_fibonacci_numbers.inject(0) {|sum, x| sum + x}
+	end
+
+	def even_fibonacci_numbers
+		array = [1, 2]
+		while array[-1] < @limit
+			array << array[-1] + array[-2]
+		end
+		array.select {|num| num.even? && num < @limit}
+	end
+
+end
