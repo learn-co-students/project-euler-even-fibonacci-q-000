68afd18ed682ac1fb9241030fa4d97ca78ab9859
davdkm <davd.km@gmail.com>
2016-10-11 00:01:29 -0700

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  index = 1
+  array = [1, 2]
+  while array[index] + array[index-1] <= limit
+    array << array[index] + array[index -1]
+    index+= 1
+  end
+  array.reject{|i| i.odd?}.inject(:+)
+end
