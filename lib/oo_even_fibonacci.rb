73a65740bd13f32f1e438bef1ada945b87fdfbdc
dustinlikens <dustinlikens@icloud.com>
2017-01-01 01:12:17 -0800

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -5,16 +5,16 @@ class EvenFibonacci
     @limit = limit
   end
 
-def find_fibs
-  fibs = [1,1]
-  while (new_fib = fibs[-1] + fibs[-2]) < @limit do
-    fibs << new_fib
+  def find_fibs
+    fibs = [1,1]
+    while (new_fib = fibs[-1] + fibs[-2]) < @limit do
+      fibs << new_fib
+    end
+    return fibs
   end
-  return fibs
-end
 
-def sum
-  find_fibs.inject(0) { |sum, i| i.even? ? sum + i : sum }
-end
+  def sum
+    find_fibs.inject(0) { |sum, i| i.even? ? sum + i : sum }
+  end
 
 end
