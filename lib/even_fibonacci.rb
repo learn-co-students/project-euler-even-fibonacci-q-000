63a256c3a4fedec107ef6fbcde5b550279182761
shoko furushita <shktkt219@gmail.com>
2017-01-31 13:01:33 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(limit)
+  even_array = []
+  i, first, second = 0, 0, 1
+  while i < limit
+    i = first + second
+    break if i > limit
+    even_array << i if i.even?
+    first = second
+    second = i
+  end
+  even_array.inject(:+)
+end
