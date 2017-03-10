9822f5f7794ce1bc29bf90c51f756479d91cc38a
SiamKing <thesiameseelectric@gmail.com>
2017-03-10 14:59:01 -0800

    you got fibonaccied

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  find_even(limit).inject(0, :+)
+end
+
+def find_fibonacci(limit)
+  sum = 1
+  nums = [sum]
+  sum += sum
+  begin
+    nums.push(sum)
+    sum += nums[-2]
+  end until sum >= limit
+  nums
+end
+
+def find_even(limit)
+  find_fibonacci(limit).select{|num| num % 2 === 0}
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,26 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    find_even.inject(0, :+)
+  end
+
+  def find_fibonacci
+    sum = 1
+    nums = [sum]
+    sum += sum
+    begin
+      nums.push(sum)
+      sum += nums[-2]
+    end until sum >= @limit
+    nums
+  end
+
+  def find_even
+    find_fibonacci.select{|num| num % 2 === 0}
+  end
+
+end
