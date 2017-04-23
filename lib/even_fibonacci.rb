4790b2a8f23efbbc4aa8567c16588f2a359fe940
depippo <jdepippo@gmail.com>
2017-04-22 20:25:28 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  
+  fib = [1, 2]
+
+  while fib[-1] < limit 
+    fib << fib[-1] + fib[-2]
+  end
+
+  fib.select {|num|
+    num.even? && num < limit
+  }.reduce(:+)
+
+end
