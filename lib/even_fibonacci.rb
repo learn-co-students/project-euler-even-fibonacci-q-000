15c2647ee0827982e07af60973921d8f20008d21
mirandalily <mirandaraymond@me.com>
2017-05-24 16:42:56 -0700

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(num)
+  array = [1,2]
+  n = 0
+  while array[n] < num
+    array << array[n] + array[n + 1]
+    n += 1
+  end
+  array.delete_if {|n| n.odd? || n > num}.reduce(:+)
+end
