7926a1c5e565a16cdee117270877fb5bfa2c0630
mirandalily <mirandaraymond@me.com>
2017-05-24 16:36:59 -0700

    defining functions

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -6,6 +6,13 @@ class EvenFibonacci
   end
 
   def sum
+    array = [1,2]
+    n = 0
+    while array[n] < @limit
+      array << array[n] + array[n + 1]
+      n += 1
+    end
+    array.delete_if{ |n| n.odd? || n > @limit}.reduce(:+)
   end
 
 end
