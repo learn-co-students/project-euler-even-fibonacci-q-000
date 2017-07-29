ccde57878187a4eaff0733ed2409df24c25ff417
Ann Lee <aplee29@users.noreply.github.com>
2017-07-29 13:54:22 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(limit)
+  filtered_arr = []
+  i, first, second = 0, 0, 1
+  while i < limit
+    i = first + second
+    break if i > limit
+    filtered_arr << i if i.even?
+    first = second
+    second = i
+  end
+  # filtered_array.reduce(:+)
+  filtered_arr.inject{|total, num| total + num}
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+  attr_reader :fibonacci_arr, :filtered_arr, :sum
+
+  def initialize(limit)
+    @first = 0
+    @second = 1
+    @fibonacci_arr = []
+    @filtered_arr = filtered_fibonacci_arr(limit)
+    @sum = even_fibonacci_sum
+  end
+
+  def filtered_fibonacci_arr(limit)
+    i = 0
+    while i < limit
+      i = @first + @second
+      break if i > limit
+      @fibonacci_arr << i if i.even?
+      @first = @second
+      @second = i
+    end
+    fibonacci_arr
+  end
+
+  def even_fibonacci_sum
+    # filtered_arr.reduce(:+) unless fibonacci_arr.empty?
+    filtered_arr.inject{|total, num| total + num}
+  end
+end
