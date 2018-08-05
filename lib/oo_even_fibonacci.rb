b10e8ce6ba13aacb205b4f42b2e5b74f9e6396cb
Bob Habig <rfhab2@gmail.com>
2018-08-04 21:12:07 -0400

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,21 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+    attr_reader :limit
+
+    def initialize(limit)
+      @limit = limit
+    end
+
+    def fib_array
+      fib = [1,2]
+
+      while fib[-1] + fib[-2] <= limit
+        fib << fib[-1] + fib[-2]
+      end
+      
+      fib
+    end
+
+    def sum
+      fib_array.select { |n| n % 2 == 0 }.reduce(:+)
+    end
+end
