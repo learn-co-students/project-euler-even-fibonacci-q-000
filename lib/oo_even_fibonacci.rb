7e677ebacfcd4da9178029efbad79a7752e3b863
jason mortensen <zaross1@yahoo.com>
2019-10-29 06:12:11 +0000

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  
+  def initialize(max)
+    @max = max
+  end 
+  
+  def sum 
+    sequence = [1, 2, 3]
+    current_index = 2
+    current_num = sequence[current_index]
+    while (current_num + sequence[current_index - 1]) <= @max
+      sequence.push(current_num + sequence[current_index - 1])
+      current_index += 1
+      current_num = sequence[current_index]
+       
+    end 
+    
+    return sequence.select {|num| num.even?}.sum
+    
+  end 
+  
+  
+end 
